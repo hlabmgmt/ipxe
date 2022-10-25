fe5583b72e3fbf600f4f1142b2fdd387414a1c94
Ashley Towns <mail@ashleytowns.id.au>
2022-10-26 06:08:24 +1100

    enable http sanboot

--- src/config/general.h
+++ src/config/general.h
@@ -7,7 +7,7 @@
  *
  */
 
-FILE_LICENCE ( GPL2_OR_LATER_OR_UBDL );
+FILE_LICENCE(GPL2_OR_LATER_OR_UBDL);
 
 #include <config/defaults.h>
 
@@ -26,19 +26,19 @@ FILE_LICENCE ( GPL2_OR_LATER_OR_UBDL );
  * initialisation vector, thus rendering the banner almost invisible
  * to the user.
  */
-#define BANNER_TIMEOUT		20
-#define ROM_BANNER_TIMEOUT	( 2 * BANNER_TIMEOUT )
+#define BANNER_TIMEOUT 20
+#define ROM_BANNER_TIMEOUT (2 * BANNER_TIMEOUT)
 
 /*
  * Network protocols
  *
  */
 
-#define	NET_PROTO_IPV4		/* IPv4 protocol */
+#define NET_PROTO_IPV4 /* IPv4 protocol */
 //#define NET_PROTO_IPV6	/* IPv6 protocol */
-#undef	NET_PROTO_FCOE		/* Fibre Channel over Ethernet protocol */
-#define	NET_PROTO_STP		/* Spanning Tree protocol */
-#undef	NET_PROTO_LACP		/* Link Aggregation control protocol */
+#undef NET_PROTO_FCOE /* Fibre Channel over Ethernet protocol */
+#define NET_PROTO_STP /* Spanning Tree protocol */
+#undef NET_PROTO_LACP /* Link Aggregation control protocol */
 
 /*
  * PXE support
@@ -52,12 +52,12 @@ FILE_LICENCE ( GPL2_OR_LATER_OR_UBDL );
  *
  */
 
-#define	DOWNLOAD_PROTO_TFTP	/* Trivial File Transfer Protocol */
-#define	DOWNLOAD_PROTO_HTTP	/* Hypertext Transfer Protocol */
-#define	DOWNLOAD_PROTO_HTTPS	/* Secure Hypertext Transfer Protocol */
-#undef	DOWNLOAD_PROTO_FTP	/* File Transfer Protocol */
-#undef	DOWNLOAD_PROTO_SLAM	/* Scalable Local Area Multicast */
-#undef	DOWNLOAD_PROTO_NFS	/* Network File System Protocol */
+#define DOWNLOAD_PROTO_TFTP	 /* Trivial File Transfer Protocol */
+#define DOWNLOAD_PROTO_HTTP	 /* Hypertext Transfer Protocol */
+#define DOWNLOAD_PROTO_HTTPS /* Secure Hypertext Transfer Protocol */
+#undef DOWNLOAD_PROTO_FTP	 /* File Transfer Protocol */
+#undef DOWNLOAD_PROTO_SLAM	 /* Scalable Local Area Multicast */
+#undef DOWNLOAD_PROTO_NFS	 /* Network File System Protocol */
 //#undef DOWNLOAD_PROTO_FILE	/* Local filesystem access */
 
 /*
@@ -65,11 +65,11 @@ FILE_LICENCE ( GPL2_OR_LATER_OR_UBDL );
  *
  */
 
-#undef	SANBOOT_PROTO_ISCSI	/* iSCSI protocol */
-#undef	SANBOOT_PROTO_AOE	/* AoE protocol */
-#undef	SANBOOT_PROTO_IB_SRP	/* Infiniband SCSI RDMA protocol */
-#undef	SANBOOT_PROTO_FCP	/* Fibre Channel protocol */
-#undef	SANBOOT_PROTO_HTTP	/* HTTP SAN protocol */
+#undef SANBOOT_PROTO_ISCSI	/* iSCSI protocol */
+#undef SANBOOT_PROTO_AOE	/* AoE protocol */
+#undef SANBOOT_PROTO_IB_SRP /* Infiniband SCSI RDMA protocol */
+#undef SANBOOT_PROTO_FCP	/* Fibre Channel protocol */
+#define SANBOOT_PROTO_HTTP	/* HTTP SAN protocol */
 
 /*
  * HTTP extensions
@@ -94,7 +94,7 @@ FILE_LICENCE ( GPL2_OR_LATER_OR_UBDL );
  *
  */
 
-#define	DNS_RESOLVER		/* DNS resolver */
+#define DNS_RESOLVER /* DNS resolver */
 
 /*
  * Image types
@@ -107,60 +107,60 @@ FILE_LICENCE ( GPL2_OR_LATER_OR_UBDL );
 //#define	IMAGE_ELF		/* ELF image support */
 //#define	IMAGE_MULTIBOOT		/* MultiBoot image support */
 //#define	IMAGE_PXE		/* PXE image support */
-#define	IMAGE_SCRIPT		/* iPXE script image support */
+#define IMAGE_SCRIPT /* iPXE script image support */
 //#define	IMAGE_BZIMAGE		/* Linux bzImage image support */
 //#define	IMAGE_COMBOOT		/* SYSLINUX COMBOOT image support */
-#define	IMAGE_EFI		/* EFI image support */
+#define IMAGE_EFI /* EFI image support */
 //#define	IMAGE_SDI		/* SDI image support */
 //#define	IMAGE_PNM		/* PNM image support */
-#define	IMAGE_PNG		/* PNG image support */
-#define	IMAGE_DER		/* DER image support */
-#define	IMAGE_PEM		/* PEM image support */
+#define IMAGE_PNG /* PNG image support */
+#define IMAGE_DER /* DER image support */
+#define IMAGE_PEM /* PEM image support */
 
 /*
  * Command-line commands to include
  *
  */
-#define	AUTOBOOT_CMD		/* Automatic booting */
-#define	NVO_CMD			/* Non-volatile option storage commands */
-#define	CONFIG_CMD		/* Option configuration console */
-#define	IFMGMT_CMD		/* Interface management commands */
+#define AUTOBOOT_CMD /* Automatic booting */
+#define NVO_CMD		 /* Non-volatile option storage commands */
+#define CONFIG_CMD	 /* Option configuration console */
+#define IFMGMT_CMD	 /* Interface management commands */
 //#define	IWMGMT_CMD		/* Wireless interface management commands */
 //#define IBMGMT_CMD		/* Infiniband management commands */
 //#define FCMGMT_CMD		/* Fibre Channel management commands */
-#define	ROUTE_CMD		/* Routing table management commands */
-#define IMAGE_CMD		/* Image management commands */
-#define DHCP_CMD		/* DHCP management commands */
-//#define SANBOOT_CMD		/* SAN boot commands */
-#define MENU_CMD		/* Menu commands */
+#define ROUTE_CMD	/* Routing table management commands */
+#define IMAGE_CMD	/* Image management commands */
+#define DHCP_CMD	/* DHCP management commands */
+#define SANBOOT_CMD /* SAN boot commands */
+#define MENU_CMD	/* Menu commands */
 //#define LOGIN_CMD		/* Login command */
-#define SYNC_CMD		/* Sync command */
-#define SHELL_CMD		/* Shell command */
+#define SYNC_CMD  /* Sync command */
+#define SHELL_CMD /* Shell command */
 //#define NSLOOKUP_CMD		/* DNS resolving command */
-#define TIME_CMD		/* Time commands */
+#define TIME_CMD /* Time commands */
 //#define DIGEST_CMD		/* Image crypto digest commands */
 //#define LOTEST_CMD		/* Loopback testing commands */
-#define VLAN_CMD		/* VLAN commands */
+#define VLAN_CMD /* VLAN commands */
 //#define PXE_CMD		/* PXE commands */
-#define REBOOT_CMD		/* Reboot command */
-#define POWEROFF_CMD		/* Power off command */
+#define REBOOT_CMD	 /* Reboot command */
+#define POWEROFF_CMD /* Power off command */
 //#define IMAGE_TRUST_CMD	/* Image trust management commands */
 //#define PCI_CMD		/* PCI commands */
 //#define PARAM_CMD		/* Form parameter commands */
 //#define NEIGHBOUR_CMD		/* Neighbour management commands */
-#define PING_CMD		/* Ping command */
-#define CONSOLE_CMD		/* Console command */
+#define PING_CMD	/* Ping command */
+#define CONSOLE_CMD /* Console command */
 //#define IPSTAT_CMD		/* IP statistics commands */
 //#define PROFSTAT_CMD		/* Profiling commands */
 //#define NTP_CMD		/* NTP commands */
-#define CERT_CMD		/* Certificate management commands */
+#define CERT_CMD /* Certificate management commands */
 
 /*
  * ROM-specific options
  *
  */
-#undef	NONPNP_HOOK_INT19	/* Hook INT19 on non-PnP BIOSes */
-#define	AUTOBOOT_ROM_FILTER	/* Autoboot only devices matching our ROM */
+#undef NONPNP_HOOK_INT19	/* Hook INT19 on non-PnP BIOSes */
+#define AUTOBOOT_ROM_FILTER /* Autoboot only devices matching our ROM */
 
 /*
  * Virtual network devices
@@ -173,7 +173,7 @@ FILE_LICENCE ( GPL2_OR_LATER_OR_UBDL );
  * Error message tables to include
  *
  */
-#undef	ERRMSG_80211		/* All 802.11 error descriptions (~3.3kb) */
+#undef ERRMSG_80211 /* All 802.11 error descriptions (~3.3kb) */
 
 /*
  * Obscure configuration options
@@ -182,20 +182,20 @@ FILE_LICENCE ( GPL2_OR_LATER_OR_UBDL );
  *
  */
 
-#undef	BUILD_SERIAL		/* Include an automatic build serial
-				 * number.  Add "bs" to the list of
-				 * make targets.  For example:
-				 * "make bin/rtl8139.dsk bs" */
-#undef	BUILD_ID		/* Include a custom build ID string,
-				 * e.g "test-foo" */
-#undef	NULL_TRAP		/* Attempt to catch NULL function calls */
-#undef	GDBSERIAL		/* Remote GDB debugging over serial */
-#undef	GDBUDP			/* Remote GDB debugging over UDP
-				 * (both may be set) */
+#undef BUILD_SERIAL /* Include an automatic build serial \
+					 * number.  Add "bs" to the list of  \
+					 * make targets.  For example:       \
+					 * "make bin/rtl8139.dsk bs" */
+#undef BUILD_ID		/* Include a custom build ID string, \
+					 * e.g "test-foo" */
+#undef NULL_TRAP	/* Attempt to catch NULL function calls */
+#undef GDBSERIAL	/* Remote GDB debugging over serial */
+#undef GDBUDP		/* Remote GDB debugging over UDP \
+					 * (both may be set) */
 //#define EFI_DOWNGRADE_UX	/* Downgrade UEFI user experience */
-#define	TIVOLI_VMM_WORKAROUND	/* Work around the Tivoli VMM's garbling of SSE
-				 * registers when iPXE traps to it due to
-				 * privileged instructions */
+#define TIVOLI_VMM_WORKAROUND /* Work around the Tivoli VMM's garbling of SSE \
+							   * registers when iPXE traps to it due to       \
+							   * privileged instructions */
 
 #include <config/named.h>
 #include NAMED_CONFIG(general.h)
